be4128796955cac0fed706f78b4875ebfc7ab14f
Devin Corrigall <devincorrigall@gmail.com>
2015-12-16 23:13:20 -0800

    Fixed Battery indicator

--- src/main.c
+++ src/main.c
@@ -10,7 +10,8 @@ static GFont s_rwby_date_font;
 static int s_battery_level;
 static Layer *s_battery_layer;
 static Layer *s_battery_background_layer;
-
+static BitmapLayer *s_background_layer, *s_bt_icon_layer;
+static GBitmap *s_background_bitmap, *s_bt_icon_bitmap;
 
 static void update_time() {
     time_t temp = time(NULL); 
@@ -46,7 +47,7 @@ static void battery_update_proc(Layer *layer, GContext *ctx) {
     GRect bounds = layer_get_bounds(layer);
 
     // Find the width of the bar
-    int width = (int)(float)(((float)s_battery_level / 100.0F) * 114.0F);
+    int width = (int)(float)(((float)s_battery_level / 100.0F) * bounds.size.w);
 
     // Draw the background
     graphics_context_set_fill_color(ctx, GColorWhite);
@@ -68,6 +69,16 @@ static void battery_callback(BatteryChargeState state) {
     layer_mark_dirty(s_battery_layer);
 }
 
+static void bluetooth_callback(bool connected) {
+  // Show icon if disconnected
+  layer_set_hidden(bitmap_layer_get_layer(s_bt_icon_layer), connected);
+
+  if(!connected) {
+    // Issue a vibrating alert
+    vibes_double_pulse();
+  }
+}
+
 static void main_window_load(Window *window) {
     // Get information about the Window
     Layer *window_layer = window_get_root_layer(window);
@@ -84,7 +95,7 @@ static void main_window_load(Window *window) {
     s_rwby_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_RWBY_DATE_FONT_20));
 
     // Show time
-    s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(10, 5), bounds.size.w, 50));
+    s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(10, 2), bounds.size.w, 50));
     text_layer_set_font(s_time_layer, s_rwby_time_font);
     text_layer_set_background_color(s_time_layer, GColorClear);
     text_layer_set_text_color(s_time_layer, GColorBlack);
@@ -105,7 +116,14 @@ static void main_window_load(Window *window) {
     layer_add_child(window_get_root_layer(window), s_battery_background_layer);
     s_battery_layer = layer_create(GRect(PBL_IF_ROUND_ELSE(25, 15), PBL_IF_ROUND_ELSE(136, 131), bounds.size.w - PBL_IF_ROUND_ELSE(50, 30), 4));
     layer_set_update_proc(s_battery_layer, battery_update_proc);
-    layer_add_child(window_get_root_layer(window), s_battery_layer);
+    layer_add_child(window_layer, s_battery_layer);
+
+    // Setup bluetooth indicator
+    s_bt_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BT_ICON);
+    s_bt_icon_layer = bitmap_layer_create(GRect(PBL_IF_ROUND_ELSE(15, 10), 100, 20, 20));
+    bitmap_layer_set_bitmap(s_bt_icon_layer, s_bt_icon_bitmap);
+    layer_add_child(window_layer, bitmap_layer_get_layer(s_bt_icon_layer));
+    bluetooth_callback(connection_service_peek_pebble_app_connection());
 }
 
 static void main_window_unload(Window *window) {
@@ -118,8 +136,9 @@ static void main_window_unload(Window *window) {
     bitmap_layer_destroy(s_background_layer);
     layer_destroy(s_battery_layer);
     layer_destroy(s_battery_background_layer);
+    gbitmap_destroy(s_bt_icon_bitmap);
+    bitmap_layer_destroy(s_bt_icon_layer);
 }
-
 
 static void init() {
     // Create main Window
@@ -145,6 +164,11 @@ static void init() {
 
     // Register for battery level updates
     battery_state_service_subscribe(battery_callback);
+
+    // Register for Bluetooth connection updates
+    connection_service_subscribe((ConnectionHandlers) {
+        .pebble_app_connection_handler = bluetooth_callback
+    });
 }
 
 static void deinit() {
